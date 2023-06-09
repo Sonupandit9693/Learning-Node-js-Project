ddb982945bfe48991ad106422ed5ca91fce898e5
sonu kumar <sonupandit969371@gmail.com>
2023-06-09 16:02:35 +0530

    blog

--- Blog-app/models/user.js
+++ Blog-app/models/user.js
@@ -1,6 +1,7 @@
 const { error } = require("console");
 const { createHmac, randomBytes } = require("crypto");
 const { Schema, default: mongoose } = require("mongoose");
+const { createTokenForUser } = require("../services/authentication");
 
 const userSchema = Schema(
   {
@@ -44,26 +45,28 @@ userSchema.pre("save", function (next) {
     .digest("hex");
   this.salt = salt;
   this.password = hashedPassword;
-
   next();
 });
 
-userSchema.static("matchPassword", async function (email, password) {
-  const user = await this.findOne({ email });
-  console.log(user);
-  if (!user) throw new Error("user not found");
+userSchema.static(
+  "matchPasswordAndGenrateToken",
+  async function (email, password) {
+    const user = await this.findOne({ email });
+    // console.log(user);
+    if (!user) return console.log("user not found");
 
-  const salt = user.salt;
-  const hashedPassword = user.password;
+    const salt = user.salt;
+    const hashedPassword = user.password;
 
-  const userProvideHash = createHmac("sha256", salt)
-    .update(user.password)
-    .digest("hex");
+    const userProvideHash = createHmac("sha256", salt)
+      .update(password)
+      .digest("hex");
 
-  if (hashedPassword !== userProvideHash) throw new Error("Incorrect password");
-
-  return { ...user, password: undefined, salt: undefined };
-});
+    if (hashedPassword !== userProvideHash) throw console.error("Incorrect password");
+    const token = createTokenForUser(user);
+    return token;
+  }
+);
 
 const User = mongoose.model("user", userSchema);
 
